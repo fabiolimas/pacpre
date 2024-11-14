6958fe122087260334c8c0000626d99026f5884f
Fábio Lima <fabiolima01@live.com>
2024-11-14 09:17:32 -0300

    att 2 grafico novo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -59,7 +59,7 @@ class HomeController extends Controller
             $cartoesVendidosLojas = Venda::join('lojas','lojas.id','vendas.loja_id')
             ->where('status', 'Vendido')
                 ->whereBetween('vendas.created_at', [$dataInicio, $dataFim])
-                ->groupBy('loja_id', DB::raw('DATE(vendas.created_at)'))
+                ->groupBy('loja_id','lojas.nfantasia', DB::raw('DATE(vendas.created_at)'))
                 ->select('loja_id','lojas.nfantasia', DB::raw('DATE(vendas.created_at) as dia'), DB::raw('COUNT(*) as total_cartoes'))
                 ->get();
 
